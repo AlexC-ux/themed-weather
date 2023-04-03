92f3165f2faaf302681848598e93a906d6f132e9
AlexC-ux <eunv2004@gmail.com>
2023-04-04 02:26:34 +0300

    corners added

--- components/WeatherDisplay/index.tsx
+++ components/WeatherDisplay/index.tsx
@@ -1,8 +1,9 @@
-import { createRef, useContext, useEffect } from "react"
+import { createRef, useContext, useEffect, useState } from "react"
 import styles from "./weatherdisplay.module.scss"
 import { locationContext } from "@/contextes/LocationContext"
 
-export interface IWeather {
+
+export interface Root {
     latitude: number
     longitude: number
     generationtime_ms: number
@@ -10,12 +11,14 @@ export interface IWeather {
     timezone: string
     timezone_abbreviation: string
     elevation: number
-    current_weather: ICurrentWeather
-    hourly_units: IHourlyUnits
-    hourly: IHourly
+    current_weather: CurrentWeather
+    hourly_units: HourlyUnits
+    hourly: Hourly
+    daily_units: DailyUnits
+    daily: Daily
 }
 
-export interface ICurrentWeather {
+export interface CurrentWeather {
     temperature: number
     windspeed: number
     winddirection: number
@@ -23,43 +26,86 @@ export interface ICurrentWeather {
     time: string
 }
 
-export interface IHourlyUnits {
+export interface HourlyUnits {
     time: string
+    relativehumidity_2m: string
     temperature_2m: string
 }
 
-export interface IHourly {
+export interface Hourly {
     time: string[]
+    relativehumidity_2m: number[]
     temperature_2m: number[]
 }
 
+export interface DailyUnits {
+    time: string
+    precipitation_sum: string
+    sunrise: string
+    sunset: string
+    precipitation_probability_max: string
+}
+
+export interface Daily {
+    time: string[]
+    precipitation_sum: number[]
+    sunrise: string[]
+    sunset: string[]
+    precipitation_probability_max: number[]
+}
+
+
 
 export function WeatherDisplay() {
-    const currentTempRef = createRef<HTMLDivElement>();
+    const [weather, setWeather] = useState<Root>()
 
     const location = useContext(locationContext)
 
     useEffect(() => {
-        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${location.location.latitude}&longitude=${location.location.longitude}&hourly=temperature_2m&current_weather=true&forecast_days=2&timezone=Europe/Moscow`).then(data => {
-            data.json().then((weather: IWeather) => {
-                currentTempRef.current!.innerText = weather.current_weather.temperature.toString()
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${location.location.latitude}&longitude=${location.location.longitude}&hourly=relativehumidity_2m,temperature_2m&current_weather=true&daily=precipitation_sum,sunrise,sunset,precipitation_probability_max&forecast_days=2&timezone=Europe/Moscow`).then(data => {
+            data.json().then((weather: Root) => {
+                setWeather(weather);
             })
         })
     }, [location.location])
     return <>
         <div className={styles.displayContainer}>
-            <div
-                className={styles.currentWeather}>
+            <div className={styles.currentWeather}>
                 <div>
+                    <div
+                        className={styles.degr}>
+                        {weather?.current_weather.temperature}
+                    </div>
                     <div className={styles.city}>
                         {location.location.name}
                     </div>
-                    <div
-                        className={styles.degr}
-                        ref={currentTempRef}>
-
+                    <div>
+                        <div className={styles.params}>
+                            <div className={styles.windSpeed}>
+                                <span className="material-symbols-outlined">
+                                    air
+                                </span>{weather?.current_weather.windspeed}
+                            </div>
+                            <div className={styles.precipitation}>
+                                <span className="material-symbols-outlined">
+                                    umbrella
+                                </span>{weather?.daily.precipitation_sum[0]}
+                            </div>
+                            <div className={styles.precipitationProbMax}>
+                                <span className="material-symbols-outlined">
+                                    rainy
+                                </span>{weather?.daily.precipitation_probability_max[0]}
+                            </div>
+                            <div className={styles.humidity}>
+                                <span className="material-symbols-outlined">
+                                    airwave
+                                </span>{weather?.hourly.relativehumidity_2m[new Date().getHours()]}
+                            </div>
+                        </div>
                     </div>
                 </div>
+            </div>
+            <div className={styles.nextHourWeather}>
             </div>
         </div>
     </>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '@/styles/normalize.css'
+import '@/styles/global.css'
+import 'material-icons/iconfont/material-icons.css';
 import type { AppProps } from 'next/app'
 import React from 'react'
 
