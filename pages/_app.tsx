599e5fa03415de1c8c8faa10d1f0d47c09e6e424
AlexC-ux <eunv2004@gmail.com>
2023-04-04 02:39:09 +0300

    font fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '@/styles/normalize.css'
 import '@/styles/global.css'
-import 'material-icons/iconfont/material-icons.css';
 import type { AppProps } from 'next/app'
 import React from 'react'
 
