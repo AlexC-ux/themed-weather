68358e7a8d42deb408bbf33fac1592ff8ce6c69c
AlexC-ux <eunv2004@gmail.com>
2023-04-04 20:57:20 +0300

    card color change added

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -1,14 +1,28 @@
+'use-client'
 import { MdOutlineSettings } from "react-icons/md"
 import { CitySearch } from "../CitySearch"
 import styles from "./menu.module.scss"
-import { createRef, useState } from "react"
+import { createRef, useEffect, useState } from "react"
 
+
+type cssParamType = 'searchResultBg' | 'serachBackground' | 'currentWeatherCardBg' | 'currentWeatherCityColor'
+    | 'currentWeatherTempColor' | 'currentWeatherCardShadow' | 'currentDateTextColor' |
+    'currentWeatherSunriseColor' | 'currentWeatherSunsetColor' | 'currentAppearentWeatherTempTextColor' |
+    'currentWeatherWindTextColor' | 'currentWeatherWindBgColor' | 'WeatherCodeIconColor' | 'currentWeatherPrecipitationBgColor'
+    | 'currentWeatherPrecipitationTextColor' | 'currentWeatherPrecipitationProbMaxBgColor' |
+    'currentWeatherHumidityBgColor' | 'currentWeatherHumidityTextColor' | 'currentWeatherCelsiumColor' |
+    'BGCOLOR' | 'currentWeatherPrecipitationProbMaxTextColor'
 
 export function Menu() {
 
     const [menuShown, setMenuShow] = useState(false);
 
-    const [iconClasses, setIconClasses] = useState(`${styles.menuIcon}`)
+    const [iconClasses, setIconClasses] = useState(`${styles.menuIcon}`);
+
+    function updateParam(props: { paramName: cssParamType, value: string }) {
+        document.documentElement.style.setProperty(`--${props.paramName}`, props.value)
+        console.log(props)
+    }
 
     function showMenu() {
         if (!menuShown) {
@@ -20,6 +34,51 @@ export function Menu() {
             setIconClasses(`${styles.menuIcon}`)
             document.documentElement.style.overflow = "scroll"
         }
+    }
+
+
+    function UpdateParamField(props: { text: string, paramName: cssParamType }) {
+        const inputRef = createRef<HTMLInputElement>();
+
+        useEffect(() => {
+            const propValue = getComputedStyle(document.documentElement).getPropertyValue(`--${props.paramName}`).trim();
+            console.log({ propValue })
+            if (propValue.startsWith("#")) {
+                inputRef.current!.setAttribute("value", propValue)
+            } else if (propValue.startsWith("rgb")) {
+                const vals = /rgb\((\d{1,3})[, ]+(\d{1,3})[, ]+(\d{1,3})\)/gm.exec(propValue)!
+                console.log({ vals })
+                const hexValue = rgbToHex(Number(vals[1]), Number(vals[2]), Number(vals[3]))
+                console.log({ r: vals[1], g: vals[2], b: vals[3] })
+                inputRef.current!.setAttribute("value", hexValue)
+            } else {
+                console.log("no rule")
+            }
+        })
+
+
+
+        function rgbToHex(red: number, green: number, blue: number) {
+            function ColorToHex(color: number) {
+                var hexadecimal = color.toString(16);
+                console.log({ color, hexadecimal })
+                return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+            }
+            return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue);
+        }
+
+
+        return <div className={`${styles.menuItem}`}>
+            <div className={`${styles.menuItemText}`}>
+                {props.text}
+            </div>
+            <div>
+                <input
+                    type="color"
+                    ref={inputRef}
+                    onChange={(windInput) => { updateParam({ paramName: props.paramName, value: windInput.currentTarget.value }) }} />
+            </div>
+        </div>
     }
 
 
@@ -36,9 +95,40 @@ export function Menu() {
             style={{
                 display: !!menuShown ? "block" : "none"
             }}>
-                <div className={styles.menuItems}>
+            <div className={styles.menuItems}>
+                <UpdateParamField text="Фон скорости ветра" paramName='currentWeatherWindBgColor' />
+                <UpdateParamField text="Цвет скорости ветра" paramName='currentWeatherWindTextColor' />
 
-                </div>
+                <UpdateParamField text="Фон количества осадков" paramName="currentWeatherPrecipitationBgColor" />
+                <UpdateParamField text="Цвет количества осадков" paramName="currentWeatherPrecipitationTextColor" />
+
+
+                <UpdateParamField text="Фон вероятности осадков" paramName="currentWeatherPrecipitationProbMaxBgColor" />
+                <UpdateParamField text="Цвет вероятности осадков" paramName="currentWeatherPrecipitationProbMaxTextColor" />
+
+                <UpdateParamField text="Фон влажности" paramName="currentWeatherHumidityBgColor" />
+                <UpdateParamField text="Цвет влажности" paramName="currentWeatherHumidityTextColor" />
+
+
+                <UpdateParamField text="Цвет температуры" paramName="currentWeatherTempColor" />
+                <UpdateParamField text="Цвет градусов цельсия" paramName="currentWeatherCelsiumColor" />
+
+                <UpdateParamField text="Цвет даты" paramName="currentDateTextColor" />
+
+                <UpdateParamField text="Цвет ощущаемой температуры" paramName="currentAppearentWeatherTempTextColor" />
+
+                <UpdateParamField text="Цвет города" paramName="currentWeatherCityColor" />
+
+                <UpdateParamField text="Цвет рассвета" paramName="currentWeatherSunriseColor" />
+
+                <UpdateParamField text="Цвет заката" paramName="currentWeatherSunsetColor" />
+
+                <UpdateParamField text="Цвет иконки погоды" paramName="WeatherCodeIconColor" />
+
+                <UpdateParamField text="Фон карточки погоды" paramName="currentWeatherCardBg" />
+            </div>
         </div>
     </div>
 }
+
+
