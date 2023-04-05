206af5e398a5a74fd9a500abde37e161fd6f2fb9
AlexC-ux <eunv2004@gmail.com>
2023-04-05 21:41:41 +0300

    analytics fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import '@/styles/normalize.css'
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
 import React from 'react'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+    <Component {...pageProps} />
+    <Analytics />
+  </>)
 }
